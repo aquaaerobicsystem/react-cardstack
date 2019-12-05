5e9ba14813411d212de1b12728fbfcec5dff07f9
AquaDevelopers <myaquaapps@gmail.com>
2019-12-05 08:13:43 -0600

    Update bundle.js

--- example/bundle.js
+++ example/bundle.js
@@ -22855,7 +22855,7 @@ module.exports = ReactMount.renderSubtreeIntoContainer;
 
 exports.__esModule = true;
 exports.default = void 0;
-import {people} from './people.js';
+const people =  require('./people'); 
 var _default = people;
 exports.default = _default;
 
